781ddbfb340a6e83528e8b22413c62ddf530c672
Daniel Melo <daniel.melo42@outlook.com>
2023-01-13 21:25:48 -0700

    visual indication of the login screen

--- app/src/main/java/com/study/reader/screens/login/LoginScreen.kt
+++ app/src/main/java/com/study/reader/screens/login/LoginScreen.kt
@@ -1,9 +1,10 @@
 package com.study.reader.screens.login
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun LoginScreen(navController: NavController) {
-
+    Text(text = "Login")
 }
