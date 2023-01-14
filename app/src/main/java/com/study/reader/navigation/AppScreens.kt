e48c62e54f7e0e88129fe372f10a40f870bb3d77
Daniel Melo <daniel.melo42@outlook.com>
2023-01-13 19:53:45 -0700

    fromRoute function implemented

--- app/src/main/java/com/study/reader/navigation/AppScreens.kt
+++ app/src/main/java/com/study/reader/navigation/AppScreens.kt
@@ -3,9 +3,27 @@ package com.study.reader.navigation
 enum class AppScreens {
     SplashScreen,
     LoginScreen,
+    SignupScreen,
     HomeScreen,
     SearchScreen,
     StatsScreen,
     UpdateScreen,
-    BookDetailsScreen
+    DetailsScreen;
+
+    companion object {
+        fun fromRoute(route: String?): AppScreens =
+            when(route?.substringBefore("/")) {
+                SplashScreen.name -> SplashScreen
+                LoginScreen.name -> LoginScreen
+                SignupScreen.name -> SignupScreen
+                HomeScreen.name -> HomeScreen
+                SearchScreen.name -> SearchScreen
+                DetailsScreen.name -> DetailsScreen
+                UpdateScreen.name -> UpdateScreen
+                StatsScreen.name -> StatsScreen
+                null -> HomeScreen
+                else -> throw IllegalArgumentException("Route: $route is not recognized")
+            }
+    }
+
 }
