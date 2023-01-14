e565fb95027c6b1272ae4dfa973a67c58e3ae3e3
Daniel Melo <daniel.melo42@outlook.com>
2023-01-13 20:51:54 -0700

    navigation added to main activity

--- app/src/main/java/com/study/reader/MainActivity.kt
+++ app/src/main/java/com/study/reader/MainActivity.kt
@@ -3,13 +3,16 @@ package com.study.reader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.study.reader.navigation.AppNavigation
 import com.study.reader.ui.theme.ReaderTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,28 +21,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ReaderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ReaderApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ReaderApp() {
+    ReaderTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AppNavigation()
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ReaderTheme {
-        Greeting("Android")
     }
 }
