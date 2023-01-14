1234a378987dd151034709a0095654c3261ff870
Daniel Melo <daniel.melo42@outlook.com>
2023-01-13 19:22:08 -0700

    dependency injection structure implemented

--- app/src/test/java/com/study/reader/ExampleUnitTest.kt
+++ app/src/test/java/com/study/reader/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.study.reader
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
