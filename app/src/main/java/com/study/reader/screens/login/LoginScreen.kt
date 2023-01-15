328a1eae47443d7240d232fb8b3e3e6386a6312d
Daniel Melo <daniel.melo42@outlook.com>
2023-01-14 21:17:55 -0700

    user form ui implementation finished

--- app/src/main/java/com/study/reader/screens/login/LoginScreen.kt
+++ app/src/main/java/com/study/reader/screens/login/LoginScreen.kt
@@ -1,10 +1,138 @@
 package com.study.reader.screens.login
 
-import androidx.compose.material.Text
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.study.reader.R
+import com.study.reader.components.AppLogo
+import com.study.reader.components.EmailInput
+import com.study.reader.components.PasswordInput
 
 @Composable
 fun LoginScreen(navController: NavController) {
-    Text(text = "Login")
+    val isSignupScreen = rememberSaveable() {
+        mutableStateOf(false)
+    }
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            AppLogo(modifier = Modifier.padding(bottom = 16.dp))
+            UserForm(loading = false, isCreateAccount = isSignupScreen.value) { email, password ->
+                Log.d("TAG", "LoginScreen: $email and $password")
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+            Row(
+                modifier = Modifier.padding(15.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                val secondaryActionText = if (isSignupScreen.value) "Sign up" else "Login"
+                Text(text = "New User?")
+                Text(text = secondaryActionText, modifier = Modifier
+                    .clickable {
+                        isSignupScreen.value = !isSignupScreen.value
+                    }
+                    .padding(start = 10.dp),
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colors.secondaryVariant
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, password -> }
+) {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+    val modifier = Modifier
+        .height(250.dp)
+        .background(MaterialTheme.colors.background)
+        .verticalScroll(
+            rememberScrollState()
+        )
+        .padding(horizontal = 10.dp)
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        if (isCreateAccount) {
+            Text(text = stringResource(id = R.string.create_account_password_message))
+        }
+        EmailInput(emailState = email, enabled = !loading, onAction = KeyboardActions {
+            passwordFocusRequest.requestFocus()
+        })
+
+        PasswordInput(
+            modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
+            imeAction = ImeAction.Done,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+                keyboardController?.hide()
+            })
+
+        SubmitButton(
+            text = if (isCreateAccount) "Create Account" else "Login",
+            loading = loading,
+            validInputs = valid
+        ) {
+            onDone(email.value, password.value)
+            keyboardController?.hide()
+        }
+    }
+
+}
+
+@Composable
+fun SubmitButton(text: String, loading: Boolean, validInputs: Boolean, onClick: () -> Unit) {
+    Button(
+        onClick = { onClick.invoke() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp),
+        shape = CircleShape,
+        enabled = !loading && validInputs
+    ) {
+        if (loading) CircularProgressIndicator(modifier = Modifier.size(25.dp))
+        else Text(text = text, modifier = Modifier.padding(5.dp))
+    }
 }
