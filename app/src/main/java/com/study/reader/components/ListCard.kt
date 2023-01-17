9e67faf9dea86f19a7ff6bdf0552922d97bb469d
Daniel Melo <daniel.melo42@outlook.com>
2023-01-17 11:21:00 -0700

    list card code refactor

---
+++ app/src/main/java/com/study/reader/components/ListCard.kt
@@ -0,0 +1,95 @@
+package com.study.reader.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.study.reader.model.MBook
+import com.study.reader.screens.home.BookRating
+
+@Preview
+@Composable
+fun ListCard(
+    book: MBook = MBook(
+        id = "book_id",
+        title = "Running",
+        authors = "Daniel Melo",
+        notes = "this is a great book",
+    ),
+    onClick: (String) -> Unit = {}
+) {
+    val displayMetrics = LocalContext.current.resources.displayMetrics
+    val screenWidth = displayMetrics.widthPixels / displayMetrics.density
+    val spacing = 10.dp
+
+
+    Card(
+        shape = RoundedCornerShape(30.dp),
+        backgroundColor = Color.White,
+        elevation = 6.dp,
+        modifier = Modifier
+            .padding(16.dp)
+            .height(242.dp)
+            .width(202.dp)
+            .clickable { onClick.invoke(book.title.toString()) }
+    ) {
+        Column(
+            modifier = Modifier.width(screenWidth.dp - (spacing * 2)),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Row(horizontalArrangement = Arrangement.Center) {
+                AsyncImage(
+                    model = "",
+                    contentDescription = "Book image",
+                    modifier = Modifier
+                        .height(140.dp)
+                        .width(100.dp)
+                        .padding(4.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(50.dp))
+                Column(
+                    modifier = Modifier.padding(top = 25.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.FavoriteBorder,
+                        contentDescription = "Favorite icon",
+                        modifier = Modifier.padding(bottom = 1.dp)
+                    )
+                    BookRating(score = 3.5)
+                }
+            }
+
+            Text(
+                text = book.title!!,
+                modifier = Modifier.padding(4.dp),
+                fontWeight = FontWeight.Bold,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Authors: ${book.authors!!}",
+                modifier = Modifier.padding(4.dp),
+                style = MaterialTheme.typography.caption
+            )
+        }
+    }
+}
