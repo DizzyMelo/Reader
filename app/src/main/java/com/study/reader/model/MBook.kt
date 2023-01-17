e588ed5190c1418d0a48edc3026c76652f3578d2
Daniel Melo <daniel.melo42@outlook.com>
2023-01-17 09:51:42 -0700

    Book model created

---
+++ app/src/main/java/com/study/reader/model/MBook.kt
@@ -0,0 +1,10 @@
+package com.study.reader.model
+
+data class MBook(
+    var id: String? = null,
+    var title: String? = null,
+    var authors: String? = null,
+    var notes: String? = null
+) {
+
+}
