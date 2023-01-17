d4c12404fbfd71646ae505f3a9c675df729904de
Daniel Melo <daniel.melo42@outlook.com>
2023-01-17 08:57:27 -0700

    top app bar and fab added

--- app/src/main/java/com/study/reader/screens/home/HomeScreen.kt
+++ app/src/main/java/com/study/reader/screens/home/HomeScreen.kt
@@ -1,9 +1,44 @@
 package com.study.reader.screens.home
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import com.study.reader.components.ReaderAppBar
+import com.study.reader.utils.AppColors
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ReaderAppBar(title = "A.Reader", navController = navController) },
+        floatingActionButton = {
+            FABContent(onTap = {})
+        }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
 
+        }
+    }
+
+}
+
+@Composable
+fun FABContent(onTap: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap.invoke() },
+        shape = CircleShape,
+        backgroundColor = AppColors.LightBlue
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add a book",
+            tint = Color.White
+        )
+    }
 }
