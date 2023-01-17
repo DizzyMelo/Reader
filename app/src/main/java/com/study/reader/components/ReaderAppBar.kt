44c8ab5e8f198425990cd92bc41249ce8ed93ed9
Daniel Melo <daniel.melo42@outlook.com>
2023-01-17 08:56:17 -0700

    reusable top app bar created

---
+++ app/src/main/java/com/study/reader/components/ReaderAppBar.kt
@@ -0,0 +1,63 @@
+package com.study.reader.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import com.study.reader.navigation.AppScreens
+
+@Composable
+fun ReaderAppBar(title: String, showProfile: Boolean = true, navController: NavController) {
+    TopAppBar(
+        title = {
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (showProfile) {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Logo Icon",
+                        modifier = Modifier
+                            .clip(shape = RoundedCornerShape(12.dp))
+                            .scale(.9f)
+                    )
+                }
+                Text(
+                    text = title,
+                    color = Color.Red.copy(alpha = 0.76f),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {
+                FirebaseAuth.getInstance().signOut().run {
+                    navController.navigate(AppScreens.LoginScreen.name)
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Logout,
+                    contentDescription = "Sign out"
+                )
+            }
+        },
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp
+    )
+}
