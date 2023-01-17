58c99c260144145a0604220b1d7dc197c839ea10
Daniel Melo <daniel.melo42@outlook.com>
2023-01-17 11:45:07 -0700

    list cards implemented as a LazyRow

--- app/src/main/java/com/study/reader/components/ReadingNowArea.kt
+++ app/src/main/java/com/study/reader/components/ReadingNowArea.kt
@@ -1,10 +1,16 @@
 package com.study.reader.components
 
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import com.study.reader.model.MBook
 
 @Composable
 fun ReadingNowArea(books: List<MBook>, navController: NavController) {
-
+    LazyRow(content = {
+        items(items = books) { book ->
+            ListCard(book)
+        }
+    })
 }
